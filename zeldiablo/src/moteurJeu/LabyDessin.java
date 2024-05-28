a5545a7f1cd39f1d22ccbe013c43aa1dc5abb278
Adrien M <adrien@mbp-de-adrien.home>
2024-05-28 10:57:32 +0200

    ajout labydessin

--- zeldiablo/src/moteurJeu/LabyDessin.java
+++ zeldiablo/src/moteurJeu/LabyDessin.java
@@ -3,6 +3,7 @@ package moteurJeu;
 import gameArkanoid.ArkanoidJeu;
 import gameArkanoid.Balle;
 import gameArkanoid.Raquette;
+import gameLaby.laby.Perso;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
@@ -20,7 +21,20 @@ public class LabyDessin implements DessinJeu {
         gc.setFill(Color.LIGHTGRAY);
         gc.fillRect(0, 0, canvas.getWidth(), canvas.getHeight());
 
-        // dessin
+        // dessin perso
+        gc.setFill(Color.BLACK);
+        Raquette raquette = arkanoid.getRaquette();
+        double px = raquette.getPx() - Raquette.RAQUETTE_TAILLE / 2;
+        double py = raquette.getPy();
+        gc.fillRect(px, py, Raquette.RAQUETTE_TAILLE, 10);
+
+        // dessin balle
+        gc.setFill(Color.RED);
+        Perso perso = jeu.getPerso();
+        double px = perso.getX();
+        double py = balle.getY();
+        gc.fillOval(bx - 5, by - 5, 10, 10);
+
 
 
         // dessin balle
